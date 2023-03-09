76f3a4c10270b9d59fa7d63ab85b65fce0695f61
KingTingTheGreat <jting@bu.edu>
2023-03-09 00:06:18 -0700

    added blit_all() function to reduce repeated code. removed some unnecessary objects

--- main.py
+++ main.py
@@ -15,16 +15,11 @@ if __name__ == '__main__':
     SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
     SCREEN.fill('white')
 
-    # set up tile colors
-    COLORS = {0:'#ffffff', 2:'#cc0001', 4:'#fb940b', 8:'#ffff01', \
-              16:'#01cc00', 32:'#03c0c6', 64:'#0000fe', 128:'#762ca7', \
-              256: '#fe98bf', 512: '#fe98bf', 1024: '#fe98bf', 2048: '#fe98bf'}
-    ALBUMS = {0: '_', 2: 'TaylorSwift', 4: 'Fearless', 8: 'SpeakNow', \
-            16: 'Red', 32: '1989', 64: 'Reputation', 128: 'Lover', \
-            256: 'folklore', 512: 'evermore', 1024: 'Midnights', 2048: 'ME!'}
-    for val in ALBUMS:
-        img = pygame.image.load(f'tiles\{ALBUMS[val]}.png').convert_alpha()
-        ALBUMS[val] = pygame.transform.scale(img, (100, 100))
+    # set up tile 
+    # COLORS = {0:'#ffffff', 2:'#cc0001', 4:'#fb940b', 8:'#ffff01', \
+    #           16:'#01cc00', 32:'#03c0c6', 64:'#0000fe', 128:'#762ca7', \
+    #           256: '#fe98bf', 512: '#fe98bf', 1024: '#fe98bf', 2048: '#fe98bf'}
+
 
 
 
@@ -40,12 +35,16 @@ if __name__ == '__main__':
     BOARD = GAME.board()
 
     # set up tiles and corresponding rects
+    ALBUMS = {0: '_', 2: 'TaylorSwift', 4: 'Fearless', 8: 'SpeakNow', \
+            16: 'Red', 32: '1989', 64: 'Reputation', 128: 'Lover', \
+            256: 'folklore', 512: 'evermore', 1024: 'Midnights', 2048: 'ME!'}
+    for val in ALBUMS:
+        img = pygame.image.load(f'tiles\{ALBUMS[val]}.png').convert_alpha()
+        ALBUMS[val] = pygame.transform.scale(img, (100, 100))
     TILE_DIM = 100
     TILE_SPACING = 10
-    TILES = {(i, j): pygame.Surface((TILE_DIM, TILE_DIM)) for j in range(DIMS[1])
-             for i in range(DIMS[0])}
-    TILES_RECTS = {(i, j): TILES[(i, j)].get_rect(center=((x_center + (TILE_DIM+TILE_SPACING)*(j-1.5), y_center + (TILE_DIM+TILE_SPACING)*(i-1.5)))) \
-                     for j in range(DIMS[1]) for i in range(DIMS[0])}
+    TILES_RECTS = {(i, j): ALBUMS[0].get_rect(center=((x_center + (TILE_DIM+TILE_SPACING)*(j-1.5), y_center + (TILE_DIM+TILE_SPACING)*(i-1.5)))) \
+                    for j in range(DIMS[1]) for i in range(DIMS[0])}
 
 
     OUTLINE_DIM = TILE_DIM*DIMS[0]+TILE_SPACING*(DIMS[0]-1)+2*TILE_SPACING
@@ -53,10 +52,12 @@ if __name__ == '__main__':
     OUTLINE.fill('black')
     OUTLINE_RECT = OUTLINE.get_rect(center=(WIDTH//2, HEIGHT//2))
 
+
 def blit_title() -> None:
     title = FONT.render('Taylor Swift 2048', True, 'black')
     title_rect = title.get_rect(center=(WIDTH//2, 100))
     SCREEN.blit(title, title_rect)
+
 
 def blit_score() -> None:
     score = FONT.render(f'Score: {str(GAME.score())}', True, 'black')
@@ -65,8 +66,17 @@ def blit_score() -> None:
 
 
 def blit_tiles(board) -> None:
-    for coord in TILES:
+    for coord in TILES_RECTS:
+        # TILES[coord] = ALBUMS[board[coord]]
         SCREEN.blit(ALBUMS[board[coord]], TILES_RECTS[coord])
+
+
+def blit_all() -> None:
+    SCREEN.blit(OUTLINE, OUTLINE_RECT)
+    blit_tiles(GAME.board())
+    blit_title()
+    blit_score()
+
 
 def process_events(only_quit=False) -> None:
     for event in pygame.event.get():
@@ -89,22 +99,17 @@ def process_events(only_quit=False) -> None:
                 pygame.quit()
                 sys.exit()
 
+
 while GAME.is_playable():
     SCREEN.fill('white')
     process_events()
-    SCREEN.blit(OUTLINE, OUTLINE_RECT)
-    blit_tiles(GAME.board())
-    blit_title()
-    blit_score()
+    blit_all()
     pygame.display.update()
     GAME.move()
     CLOCK.tick(60)
 
 while True:
     process_events(only_quit=True)
-    SCREEN.blit(OUTLINE, OUTLINE_RECT)
-    blit_tiles(GAME.board())
-    blit_title()
-    blit_score()
+    blit_all()
     pygame.display.update()
     CLOCK.tick(60)
